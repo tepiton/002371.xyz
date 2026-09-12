78442c23e04acf9e663f10ba2c081c83c17db42d
Philip Borenstein <pborenstein@gmail.com>
2026-09-12 00:40:14 -0400

    Initial commit

---
+++ content/_data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Pamphlet",
+  subtitle: "",
+  url: "https://example.com/",
+  language: "en",
+  description: "A description of this literary work",
+  author: {
+    name: "Your Name",
+  },
+  image: "",
+}
